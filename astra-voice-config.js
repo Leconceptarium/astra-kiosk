e8dce7529252a1231a14fb1bca3e6df72c26092f
Leconceptarium <toitotmax@gmail.com>
2026-09-22 23:27:19 +0200

    Update astra-voice-config.js

--- astra-voice-config.js
+++ astra-voice-config.js
@@ -1,2 +1,4 @@
 // PUBLIC: uniquement l'URL HTTPS du proxy. Jamais une clé ou un code d'accès.
-window.ASTRA_VOICE_CONFIG = { endpoint: '' };
+window.ASTRA_VOICE_CONFIG = {
+  endpoint: 'https://astra-voice-morpheus.onrender.com'
+};
